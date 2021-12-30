b1151bc836528cc40c7603a525945d2af80c7b97
Duy Lạc <lachongduy@gmail.com>
2021-12-30 23:00:45 +0700

    da hoan thanh chuc nang xoa san pham cua admin

--- admin/delete.php
+++ admin/delete.php
@@ -1,6 +1,5 @@
 <?php
 require_once './config.php';
-$id = $_GET['id'];
 $sql = "DELETE FROM book where book_id = '$id'";
 $query = mysqli_query($connect, $sql);
 header("location: ../admin/admin.php");
